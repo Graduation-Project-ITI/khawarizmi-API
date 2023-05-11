0582ad7f71a03d49f35434d79a5d935e5c2f8278
AbdallahNagy <abdallahnagy773@gmail.com>
2023-05-11 22:27:59 +0300

    commit

---
+++ khawarizmi-API/Controllers/CreateLessonController.cs
@@ -0,0 +1,33 @@
+﻿using khawarizmi.BL.Managers.Lessons;
+using khawarizmi.DAL.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging.Abstractions;
+using System.Text.Json;
+
+namespace khawarizmi_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CreateLessonController : ControllerBase
+    {
+        private readonly ILessonsManager lessonsManager;
+
+        public CreateLessonController(ILessonsManager lessonsManager)
+        {
+            this.lessonsManager = lessonsManager;
+        }
+
+        [HttpPost]
+        async public Task<IActionResult> CreateLesson([FromForm]IFormFile video, [FromForm]string metadata)
+        {
+            string videoPath = await lessonsManager.StoreVideoToUploads(video);
+            Lesson? lesson = lessonsManager.VideoMetadataToLesson(metadata, videoPath);
+
+            if (lesson is null) return BadRequest();
+            lessonsManager.AddLesson(lesson);
+
+            return Ok("all good");
+        }
+    }
+}
---
+++ khawarizmi.BL/Managers/Lessons/ILessonsManager.cs
@@ -0,0 +1,16 @@
+﻿using khawarizmi.DAL.Models;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace khawarizmi.BL.Managers.Lessons;
+
+public interface ILessonsManager
+{
+    public Task<string> StoreVideoToUploads(IFormFile video);
+    public Lesson? VideoMetadataToLesson(string metadata, string videoPath);
+    public void AddLesson(Lesson lesson);
+}
