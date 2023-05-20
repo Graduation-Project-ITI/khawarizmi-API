0e6508d9b273ade8075b1a6d2c1aa004d65be4da
AbdallahNagy <abdallahnagy773@gmail.com>
2023-05-20 23:17:23 +0300

    create admin controller

---
+++ khawarizmi-API/Controllers/AdminController/AdminController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace khawarizmi_API.Controllers.AdminController
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AdminController : ControllerBase
+    {
+
+        [HttpGet]
+        public IActionResult GetCourses()
+        {
+
+
+
+            return Ok();
+        }
+    }
+}
