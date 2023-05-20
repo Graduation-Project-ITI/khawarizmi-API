1d25025d84d56760a4db3e823dab6fa98397ecaf
AbdallahNagy <abdallahnagy773@gmail.com>
2023-05-20 23:35:09 +0300

    add courses display and users display DTOs

---
+++ khawarizmi.BL/DTOS/Courses/AdminCoursesDisplayDto.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace khawarizmi.BL.Dtos.Courses;
+
+public record AdminCoursesDisplayDto(int Id,string Name, string Publisher, int UpVotes, int DownVotes, int NetVotes, DateTime Date);
